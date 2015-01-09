49a0c45235eacaba0706d4b39cda761c91096cf0
Joridos <jonatam.ribeiro@hotmail.com>
2015-01-09 20:46:09 -0200

    init spec test files

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor', group: :production
 
 group :development, :test do
+
+  gem 'rspec-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
---
+++ spec/models/attraction_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Attraction, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  # it "orders by lower date" do
+  #   lindeman = User.create!(name: "Andy", email: "Andry@gmail.com")
+  #
+  #   supernatural = Attraction.create!(name: "Supernatural", midia: "TV", user_id: lindeman.id, display_date: "2013-12-01 22:03", created_at: Time.now, updated_at:Time.now)
+  #   grim = Attraction.create!(name: "Grim", midia: "TV", user_id: lindeman.id, display_date: "2013-12-01 20:03", created_at: Time.now, updated_at:Time.now)
+  #
+  #   expect(Attraction.attractions_new).to eq([supernatural, grim])
+  # end
+end
