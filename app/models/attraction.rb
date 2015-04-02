f381ac9900130923d369fdca2621e9b5ebfae386
Bruno Casali <brunoocasali@gmail.com>
2015-04-02 20:43:06 -0300

    Update attraction.rb

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,8 +3,8 @@ class Attraction < ActiveRecord::Base
 
   validates :name, presence: { message: 'Um nome é necessário' }
   validates :midia, presence: { message: 'Uma mídia é necessário' }
-  validates :user_id, presence: { message: 'Precisa estar relacionado a um usuário' }
+  validates :user, presence: { message: 'Precisa estar relacionado a um usuário' }
 
-  scope :attractions_new, Proc.new { |after = DateTime.now| where('display_date > ?', after).order("display_date ASC") }
-  scope :attractions_old, Proc.new { |after = DateTime.now| where('display_date < ?', after).order("display_date ASC") }
+  scope :attractions_new, -> { where(:display_date > after).order(:display_date) }
+  scope :attractions_old, -> { where(:display_date < after).order(:display_date) }
 end
